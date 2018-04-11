afce8f00d88fceb146ccd49a03c8aadab8bcb172
Turan Naimey <turan1naimey@gmail.com>
2018-04-11 11:00:38 -0700

    ENH: Set up the style guide with eslint (#28)

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,17 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env']
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel-preset-env']
+            }
+          },
+          {
+            loader: 'eslint-loader'
           }
-        }
+        ]
       }
     ]
   }
