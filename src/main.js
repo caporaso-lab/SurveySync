fcb7b96e41cd13a7927f1160530a138fb95f36e1
Turan Naimey <turan1naimey@gmail.com>
2018-04-23 13:12:47 -0700

    Work in progress: Added onOpen(e) function to add menu items when a document is opened.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import helloWorld from './utils';
+import onOpen from './utils';
 
 // In order for functions to be exposed to the Google Apps Script
 // Engine, we need to register them on the `global` context.
 //
 // See https://github.com/Automattic/google-docs-add-on
 // for more details.
-global.helloWorld = helloWorld;
+global.onOpen = onOpen;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
-export default function helloWorld() {
-  Logger.log('hello world'); // eslint-disable-line
+// eslint-disable-next-line no-unused-vars
+export default function onOpen(e) {
+  SpreadsheetApp.getUi().createAddonMenu().addItem('First Item','firstItem').addItem('Second Item','secondItem').addToUi(); // eslint-disable-line
 }
