2ca10c4e3d9da99d4b06fc92f54f7b4fb958fefa
Turan Naimey <turan1naimey@gmail.com>
2018-04-25 13:27:39 -0700

    Keeping Track: Added the populateSheet function that receives data from the HTTP server and fills the selected cells.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import onOpen from './utils';
-import populateSheet from './utils';
+
+import { onOpen, populateSheet } from './utils';
+
 // In order for functions to be exposed to the Google Apps Script
 // Engine, we need to register them on the `global` context.
-//
 // See https://github.com/Automattic/google-docs-add-on
 // for more details.
 global.onOpen = onOpen;
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,11 @@
-export default function onOpen() {
+export function onOpen() {
   SpreadsheetApp.getUi().createAddonMenu().addItem('Populate Sheet','populateSheet').addItem('Second Item','secondItem').addToUi(); // eslint-disable-line
 }
 
 export function populateSheet() {
   var rangeList = SpreadsheetApp.getActiveSpreadsheet().getActiveRangeList(); // eslint-disable-line
-  rangeList.setValue('Survey Sync'); // eslint-disable-line
+	var response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic-get'); // eslint-disable-line
+	var text = response.getContentText(); // eslint-disable-line
+  var obj = JSON.parse(text); // eslint-disable-line
+  rangeList.setValue(obj.msg); // eslint-disable-line
 }
