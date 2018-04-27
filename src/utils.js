144c5977dd042b4a994b1bfe769dea1dd0d3b7ae
Turan Naimey <turan1naimey@gmail.com>
2018-04-27 13:10:47 -0700

    SQUASH: Configured .eslintrc to ignore certain APIs. Configured travis to run on node v9.

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,17 @@
 export function onOpen() {
-  SpreadsheetApp.getUi().createAddonMenu().addItem('Populate Sheet','populateSheet').addItem('Help','help').addToUi(); // eslint-disable-line
+  SpreadsheetApp.getUi().createAddonMenu()
+    .addItem('Populate Sheet', 'populateSheet')
+    .addItem('Help', 'help')
+    .addToUi();
 }
 
 export function populateSheet() {
   const rangeList = SpreadsheetApp.getActiveSpreadsheet()
     .getActiveRangeList();
-	let response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic-get'); // eslint-disable-line
-	const text = response.getContentText();
+  const response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic-get');
+  const text = response.getContentText();
   const obj = JSON.parse(text);
   if (obj.msg === 'hello world') {
-		 rangeList.setValue(obj.msg); // eslint-disable-line
+    rangeList.setValue(obj.msg);
   }
-}
-
-export function help() {
-
-
 }
