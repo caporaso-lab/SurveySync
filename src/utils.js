aa5e7f4ddcd188a6f40bfa9290bae2784abb60a6
Turan Naimey <turan1naimey@gmail.com>
2018-04-27 12:55:59 -0700

    Keeping track

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,19 @@
 export function onOpen() {
-  SpreadsheetApp.getUi().createAddonMenu().addItem('Populate Sheet','populateSheet').addItem('Second Item','secondItem').addToUi(); // eslint-disable-line
+  SpreadsheetApp.getUi().createAddonMenu().addItem('Populate Sheet','populateSheet').addItem('Help','help').addToUi(); // eslint-disable-line
 }
 
 export function populateSheet() {
-  var rangeList = SpreadsheetApp.getActiveSpreadsheet().getActiveRangeList(); // eslint-disable-line
-	var response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic-get'); // eslint-disable-line
-	var text = response.getContentText(); // eslint-disable-line
-  var obj = JSON.parse(text); // eslint-disable-line
-  rangeList.setValue(obj.msg); // eslint-disable-line
+  const rangeList = SpreadsheetApp.getActiveSpreadsheet()
+    .getActiveRangeList();
+	let response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic-get'); // eslint-disable-line
+	const text = response.getContentText();
+  const obj = JSON.parse(text);
+  if (obj.msg === 'hello world') {
+		 rangeList.setValue(obj.msg); // eslint-disable-line
+  }
+}
+
+export function help() {
+
+
 }
