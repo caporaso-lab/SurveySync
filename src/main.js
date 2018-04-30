bd7006b92d6120062ac0bee986f86c421aa48f1a
Turan Naimey <turan1naimey@gmail.com>
2018-04-30 10:48:44 -0700

    SQUASH: Responding to @thermokarst's comments.
    Removed blank line

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-
 import { onOpen, populateSheet } from './utils';
-
 // In order for functions to be exposed to the Google Apps Script
 // Engine, we need to register them on the `global` context.
 // See https://github.com/Automattic/google-docs-add-on
