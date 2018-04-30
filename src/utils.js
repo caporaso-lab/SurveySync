d71071c09face183a7e41d5abbfc8880af20bc9f
Turan Naimey <turan1naimey@gmail.com>
2018-04-30 13:14:03 -0700

    MISC: roughing in general functionality; learning Scripts/Sheets API (#33)

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,18 @@
-export default function helloWorld() {
-  Logger.log('hello world'); // eslint-disable-line
+export function onOpen() {
+  SpreadsheetApp.getUi().createAddonMenu()
+    .addItem('Populate Sheet', 'populateSheet')
+    .addItem('Help', 'help')
+    .addToUi();
+}
+
+export function populateSheet() {
+  const rangeList = SpreadsheetApp.getActiveSpreadsheet()
+    .getActiveRangeList();
+  const response = UrlFetchApp.fetch('http://ghost.mggen.nau.edu:8081/basic/get');
+  const text = response.getContentText();
+  rangeList.setValue(text);
+  const obj = JSON.parse(text);
+  if (obj.msg === 'hello world') {
+    rangeList.setValue(obj.msg);
+  }
 }
